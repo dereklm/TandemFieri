e9c2d9323f1775e6c6208e319d0012b997824a32
apettitt9 <apettitt9@gmail.com>
2017-03-08 23:14:29 -0600

    Driver location update work (#73)

--- app/src/main/java/com/gmail/dleemcewen/tandemfieri/DinerMainMenu.java
+++ app/src/main/java/com/gmail/dleemcewen/tandemfieri/DinerMainMenu.java
@@ -76,7 +76,9 @@ public class DinerMainMenu extends AppCompatActivity {
                     }else{
                     }
                 }
-
+                return true;
+            case R.id.delivery:
+                launchDelivery();
                 return true;
             case R.id.sendSimulatedNotification:
                 //TODO: remove this after ordering and payment processing are in place
@@ -133,6 +135,16 @@ public class DinerMainMenu extends AppCompatActivity {
         //need to send user type so that the user can be located in the database
         //Bundle dinerBundle = new Bundle();
         Intent intent = new Intent(DinerMainMenu.this, DinerMapActivity.class);
+        //dinerBundle.putSerializable("User", user);
+        //intent.putExtras(dinerBundle);
+        //intent.putExtra("UserType", "Diner");
+        startActivity(intent);
+    }
+
+    private void launchDelivery(){
+        //need to send user type so that the user can be located in the database
+        //Bundle dinerBundle = new Bundle();
+        Intent intent = new Intent(DinerMainMenu.this, DeliveryMapActivity.class);
         //dinerBundle.putSerializable("User", user);
         //intent.putExtras(dinerBundle);
         //intent.putExtra("UserType", "Diner");
